9a793db9584be8c039aa6ac7d65ede81eee14e5b
Jan <malphene@gmail.com>
2021-12-27 14:07:36 -0600

    Add email and dob to database and dashboard

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -16,10 +16,14 @@ now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 def lambda_handler(event, context):
 # extract values from the event object we got from the Lambda service and store in a variable
     name = event['firstName'] +' '+ event['lastName']
+    dob = event['DOB']
+    email = event['email']
 # write name and time to the DynamoDB table using the object we instantiated and save response in a variable
     response = table.put_item(
         Item={
             'ID': name,
+            'DOB': dob,
+            'Email': email,
             'LatestGreetingTime':now
             })
 # return a properly formatted JSON object
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -5,15 +5,16 @@ const Dashboard = () => {
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [dob, setDOB] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = () => {
     if (process.env.REACT_APP_AWS_KEY) {
       const body = {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
         "firstName": firstName,
-        "lastName": lastName
+        "lastName": lastName,
+        "email": email,
+        "DOB": dob
       }
 
       axios.post(process.env.REACT_APP_AWS_KEY, body)
@@ -31,8 +32,10 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <input value={firstName} onChange={(event) => setFirstName(event.target.value)}></input>
       <input value={lastName} onChange={(event) => setLastName(event.target.value)}></input>
-      <button onClick={() => handleSubmit ()}></button>
-      <div>{firstName} {lastName} is stupid.</div>
+      <input value={dob} onChange={(event) => setDOB(event.target.value)}></input>
+      <input value={email} onChange={(event) => setEmail(event.target.value)}></input>
+      <button onClick={() => handleSubmit ()}>submit</button>
+      <div>{firstName} {lastName} born on {dob} with a the email {email} is stupid.</div>
     </>
   )
 }
